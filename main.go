62c54d741e3b3986bff1c288839226f0adeed5bd
Egon Elbre <egonelbre@gmail.com>
2018-09-28 13:05:03 +0300

    add command stubs

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+var commands = map[string]func(*State, ...string){
+	// tree prints dependency tree
+	"tree": nil,
+	// analyze size impact for each imported package
+	"size": nil,
+	// calculate with package sets
+	"calc": nil,
+	// time commands (cross-platform time)
+	"time": nil,
+}
+
+type State struct{}
+
 func main() {
 
 }
